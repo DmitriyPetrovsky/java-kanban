94c460e200e49e182af70a2191e6531cefa03565
Dmitriy Petrovskiy <d916@9054010.ru>
2024-11-21 22:18:05 +0300

    Спринт 6. Реализовать историю просмотров (пока без удаления).

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,22 +2,82 @@ package manager;
 
 import tasks.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
     private final List<Task> historyOfViews = new ArrayList<>();
+    private Map<Integer, Node> mapOfNodes = new HashMap<>();
+    private Node<Task> head = null;
+    private Node<Task> tail = null;
 
     public void add(Task task) {
         if (task != null) {
-            if (historyOfViews.size() >= 10) {
-                historyOfViews.removeFirst();
+            if (mapOfNodes.containsKey(task.getId())) {
+                mapOfNodes.remove(task.getId());
             }
-                historyOfViews.add(task);
+            Node node = new Node(task);
+            if (head == null) {
+                head = node;
+            } else if (tail == null) {
+                tail = node;
+                tail.setPrev(head);
+                head.setNext(node);
+            } else {
+                node.setPrev(tail);
+                tail.setNext(node);
+                tail = node;
+            }
         }
     }
+
     public List<Task> getHistory() {
+        Node<Task> node = head;
+        while (node.getNext() != null) {
+            historyOfViews.add(node.getValue());
+            node = node.getNext();
+        }
         return historyOfViews;
+    }
+
+    public void remove(int id) {
+
+    }
+}
+
+class Node<Task> {
+    private Task value;
+    private Node<Task> next;
+    private Node<Task> prev;
+
+    public Node(Task value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+
+    public Task getValue() {
+        return value;
+    }
+
+    public void setValue(Task value) {
+        this.value = value;
+    }
+
+    public Node<Task> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<Task> next) {
+        this.next = next;
+    }
+
+    public Node<Task> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<Task> prev) {
+        this.prev = prev;
     }
 }
